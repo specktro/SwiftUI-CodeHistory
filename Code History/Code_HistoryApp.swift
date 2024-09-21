35f27b773cdc32b7188ef0666585cae03bef714f
@specktro <specktro@nonull.mx>
2024-09-21 17:48:48 -0600

    Initial project configuration with gitignore file

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Miguel Angel Gómez Rivero on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code History/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by Miguel Angel Gómez Rivero on 21/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
