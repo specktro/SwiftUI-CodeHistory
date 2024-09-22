65cc57c39070421628cfc739ae3fd69ca7153c1b
@specktro <specktro@nonull.mx>
2024-09-22 13:03:19 -0600

    feat: Add initial question view with repetitive buttons

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            mainColor
+                .ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button {
+                        print("Tapped on Choice 1")
+                    } label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4.0)
+                            
+                    }
+                    Button {
+                        print("Tapped on Choice 2")
+                    } label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    }
+                    Button {
+                        print("Tapped on Choice 3")
+                    } label: {
+                        Text("Month")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    }
+                    Button {
+                        print("Tapped on Choice 4")
+                    } label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    }
+                }
+            }
         }
-        .padding()
+        .foregroundColor(.white)
+
     }
 }
 
