decd5d3a39e3966c5c5fdcbe0b67216c26fc9037
@specktro <specktro@nonull.mx>
2024-10-10 22:21:21 -0600

    refactor: Update the answert HStack with a view loop

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
         possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
         correctAnswerIndex: 2)
         
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     
     var body: some View {
@@ -31,25 +31,13 @@ struct ContentView: View {
                     .multilineTextAlignment(.leading)
                 Spacer()
                 HStack {
-                    Button {
-                        print("Tapped on Choice 1")
-                    } label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[0])
-                    }
-                    Button {
-                        print("Tapped on Choice 2")
-                    } label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[1])
-                    }
-                    Button {
-                        print("Tapped on Choice 3")
-                    } label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[2])
-                    }
-                    Button {
-                        print("Tapped on Choice 4")
-                    } label: {
-                        ChoiceTextView(choiceText: question.possibleAnswers[3])
+                    ForEach(question.possibleAnswers.indices, id: \.self) { index in
+                        Button {
+                            print("Tapped on option with the text: \(question.possibleAnswers[index])")
+                            mainColor = index == question.correctAnswerIndex ? .green : .red
+                        } label: {
+                            ChoiceTextView(choiceText: question.possibleAnswers[index])
+                        }
                     }
                 }
             }
