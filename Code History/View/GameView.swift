3483b327a50252700b28cb224459875316c4c944
@specktro <specktro@nonull.mx>
2024-10-13 21:24:21 -0600

    feat: Add WelcomeView and renane ContentView to GameView

--- Code History/View/GameView.swift
+++ Code History/View/GameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  GameView.swift
 //  Code History
 //
 //  Created by specktro on 21/09/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GameView: View {
     let question = Question(
         questionText: "What was the first computer bug?",
         possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
@@ -48,5 +48,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    GameView()
 }
